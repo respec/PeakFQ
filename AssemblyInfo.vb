774bee789044f354a8f6800c368f50d9b181c018
PaulHummelRESPEC <paul.hummel@respec.com>
2021-05-12 15:43:28 -0400

    Updated version number to 7.4 in all UI

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -28,6 +28,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("7.3.*")>
+<Assembly: AssemblyVersion("7.4.*")>
 
 
